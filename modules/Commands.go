500087e62e02b6729afa7d4a1e0377afbc48b793
MTplusWebSystem <mtpluswebsystem@gmail.com>
2024-03-05 19:40:26 -0400

    Projetando  o formato dos comandos

--- modules/Commands.go
+++ modules/Commands.go
@@ -4,24 +4,32 @@ import "fmt"
 
 func Rules(rules []string) {
 	var tam_rules int = len(rules)
-	if tam_rules < 2 {
-		fmt.Println("Uso: mtsound <comando> <argumento>")
-		return
-	}
 
 	rule := rules[0]
 	switch rule {
 	case "next":
 		if tam_rules == 2{
 			fmt.Printf("Indo para a música %s\n", rules[1])
+		}else{
+			fmt.Println("Indo para próxima música")
 		}
 	case "back":
-		fmt.Printf("Voltando para música %s\n", rules[1])
+		if tam_rules == 2{
+			fmt.Printf("Voltando para música %s\n", rules[1])
+		}else{
+			fmt.Println("Voltando para próxima música")
+		}
+	case "create":
+		if tam_rules == 3 && rules[1] == "playlist"{
+			fmt.Printf("Criando a playlist %s\n", rules[2])
+		} else {
+			fmt.Println("Comando create inválido. Uso: create playlist name")
+		}			
 	case "play":
 		fmt.Println("Música iniciada")
 	case "pause":
 		fmt.Println("Música pausada")
 	default:
-		fmt.Println("Comando não reconhecido.")
+		fmt.Println("Uso: mtsound <comando> <argumento>")
 	}
 }
